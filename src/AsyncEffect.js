a991ac8662019109c358e20d240155f13aae67e4
Martin Novak <ragnarecek@gmail.com>
2020-11-10 14:24:44 +1300

    removing passing value to AsyncEffect

--- src/AsyncEffect.js
+++ src/AsyncEffect.js
@@ -5,13 +5,13 @@ import {isFunction} from "./conditional";
 /**
  * AsyncEffect is a monad that allows you to safely work with asynchronous side effects in JavaScript.
  *
- * AsyncEffect expects as its input a function that takes three inputs of a reject function, a resolve function,
- * and a value. Reject function is called on failure, resolve function is called on success, and
- * value is used as an input for calculation.
+ * AsyncEffect expects as its input a function that takes two inputs of a reject function, and a resolve
+ * function. Reject function is called on failure and resolve function is called on success. It is similar
+ * to using JavaScript Promise and AsyncEffect can be directly created from a Promise turning it into a monad.
  *
  * AsyncEffect is evaluated lazily and nothing is executed until a trigger function is called.
  *
- * AsyncEffect can also be calle Future monad in other libraries or languages.
+ * AsyncEffect can also be called Future monad in other libraries or languages.
  *
  * @example
  * import {AsyncEffect, log, upperCaseOf, liftA2, liftA3} from '@7urtle/lambda';
@@ -19,13 +19,13 @@ import {isFunction} from "./conditional";
  * // we create AsyncEffect that expects a number from 0 to 1
  * // and based on that, it resolve or rejects 10 milliseconds after it is triggered
  * const myAsyncEffect = AsyncEffect
- * .of(reject => resolve => value =>
- *     setTimeout(() => value > 0.5 ? resolve('random success') : reject('random failure'), 10)
+ * .of(reject => resolve =>
+ *     setTimeout(() => Math.random() > 0.5 ? resolve('random success') : reject('random failure'), 10)
  * );
  *
  * // we could also create AsyncEffect from a JavaScript Promise
  * const myPromise = new Promise((resolve, reject) =>
- *     setTimeout(() => value > 0.5 ? resolve('random success') : reject('random failure'), 10)
+ *     setTimeout(() => Math.random() > 0.5 ? resolve('random success') : reject('random failure'), 10)
  * );
  * const promiseAsyncEffect = AsyncEffect.ofPromise(myPromise);
  *
@@ -37,13 +37,12 @@ import {isFunction} from "./conditional";
  * myAsyncEffect
  * .trigger
  * (error => log(error))
- * (result => log(result))
- * (Math.random());
+ * (result => log(result));
  * // => logs 'random success' or 'random failure' depending on Math.random() value
  *
  * // you can also turn AsyncEffect into a JavaScript Promise
  * myAsyncEffect
- * .promise(Math.random())
+ * .promise()
  * .then(result => log(result), error => log(error));
  * // => logs 'random success' or 'random failure' depending on Math.random() value
  *
@@ -52,55 +51,72 @@ import {isFunction} from "./conditional";
  * .of(() => {
  *     throw 'error';
  * })
- * .trigger(log)(log)();
+ * .trigger(log)(log);
  * // => logs 'error'
  *
  * // as a functor the value inside is safely mappable
  * // map doesn't execute in case of an error and nothing executes until a trigger is called
  * myAsyncEffect
  * .map(value => upperCaseOf(value))
- * .trigger(log)(log)(Math.random());
+ * .trigger(log)(log);
  * // => logs 'RANDOM SUCCESS' or 'random failure' depending on Math.random() value
  *
  * // as a monad AsyncEffect can be safely flat mapped with other AsyncEffects
  * // flatMap doesn't execute in case of an error and nothing executes until a trigger is called
  * AsyncEffect
- * .of(reject => resolve => value => resolve(value))
- * .flatMap(a => AsyncEffect.of(reject => resolve => value => resolve(a + 's')))
- * .trigger(log)(log)('7urtle');
+ * .of(reject => resolve => resolve('7urtle'))
+ * .flatMap(a => AsyncEffect.of(reject => resolve => resolve(a + 's')))
+ * .trigger(log)(log);
  * // => logs '7urtles'
  *
  * // as an applicative functor you can apply AsyncEffects to each other especially using liftA2 or liftA3
  * const add = a => b => a + b;
- * const AS1 = AsyncEffect.of(reject => resolve => value => resolve(1));
- * const AS2 = AsyncEffect.of(reject => resolve => value => resolve(2));
+ * const AS1 = AsyncEffect.of(reject => resolve => resolve(1));
+ * const AS2 = AsyncEffect.of(reject => resolve => resolve(2));
  * liftA2(add)(AS1)(AS2); // => resolve(3)
  *
  * const ASFail = AsyncEffect.of(() => {throw 'error'});
  * liftA3(add)(ASFail)(AS1)(AS2); // => reject('error')
  *
- * // AsyncEffect.of as well as AsyncEffect.trigger accept both curried and ternary functions
- * AsyncEffect.of((reject, resolve, value) => resolve(value)).trigger(log, log, '7urtle'); // logs '7urtle'
+ * // AsyncEffect.of as well as AsyncEffect.trigger accept both curried and binary functions
+ * AsyncEffect.of((reject, resolve) => resolve('7urtle')).trigger(log, log); // logs '7urtle'
  *
- * // as an example you can use AsyncEffect to help you work with axios
- * AsyncEffect
- * .of(reject => resolve => value => axios.get(value).then(resolve, reject))
- * .map(toUpperCaseOf)
+ * // as an example you can use AsyncEffect to help you work with axios or fs
+ *
+ * // axios example
+ * import axios from 'axios';
+ * const getFromURL = url => AsyncEffect.ofPromise(axios.get(url));
+ *
+ * getFromURL('/my/ajax/url')
  * .trigger
  * (error => log(error))
- * (result => log(response.data))
- * ('/my/ajax/url');
+ * (result => log(result.data));
+ *
+ * // reading file example
+ * import fs from 'fs';
+ * const readFile => input =>
+ *     AsyncEffect
+ *     .of(reject => resolve =>
+ *         fs.readFile(input, (err, data) =>
+ *             err ? reject(err) : resolve(data)
+ *         )
+ *     );
+ *
+ * readFile('./file.txt')
+ * .trigger
+ * (error => log(error))
+ * (result => log(result));;
  */
 export const AsyncEffect = {
-  of: trigger => getAsyncEffect(nary(reject => resolve => value => {
+  of: trigger => getAsyncEffect(nary(reject => resolve => {
     try {
-      const result = trigger(reject, resolve, value);
-      return isFunction(result) ? result(resolve)(value) : result;
+      const result = trigger(reject, resolve);
+      return isFunction(result) ? result(resolve) : result;
     } catch(error) {
       reject(error);
     }
   })),
-  ofPromise: promise => AsyncEffect.of(reject => resolve => _ =>
+  ofPromise: promise => AsyncEffect.of(reject => resolve =>
       promise.then(resolve).catch(reject)
   )
 };
@@ -108,8 +124,8 @@ export const AsyncEffect = {
 const getAsyncEffect = trigger => ({
   trigger: trigger,
   inspect: () => `AsyncEffect(${deepInspect(trigger)})`,
-  promise: value => new Promise((resolve, reject) => trigger(reject)(resolve)(value)),
-  map: fn => getAsyncEffect(nary(reject => resolve => value => trigger(reject)(a => resolve(fn(a)))(value))),
-  flatMap: fn => getAsyncEffect(nary(reject => resolve => value => trigger(reject)(x => fn(x).trigger(reject)(resolve)(value))(value))),
+  promise: () => new Promise((resolve, reject) => trigger(reject)(resolve)),
+  map: fn => getAsyncEffect(nary(reject => resolve => trigger(reject)(a => resolve(fn(a))))),
+  flatMap: fn => getAsyncEffect(nary(reject => resolve => trigger(reject)(x => fn(x).trigger(reject)(resolve)))),
   ap: f => getAsyncEffect(trigger).flatMap(fn => f.map(fn))
 });
--- tests/AsyncEffect.test.js
+++ tests/AsyncEffect.test.js
@@ -1,9 +1,9 @@
 import * as λ from '../src';
 
-const resolving = reject => resolve => value => setTimeout(() => resolve(value), 10);
-const resolvingTernary = async (reject, resolve, value) =>  setTimeout(() => resolve(value), 10);
-const rejecting = reject => resolve => value => setTimeout(() => reject('I am an error.'), 10);
-const rejectingTernary = async (reject, resolve, value) => setTimeout(() => reject('I am an error.'), 10);
+const resolving = reject => resolve => setTimeout(() => resolve('7urtle'), 10);
+const resolvingBinary = async (reject, resolve) =>  setTimeout(() => resolve('7urtle'), 10);
+const rejecting = reject => resolve => setTimeout(() => reject('I am an error.'), 10);
+const rejectingBinary = async (reject, resolve) => setTimeout(() => reject('I am an error.'), 10);
 const throwing = () => {
   throw 'I am a thrown error.';
 };
@@ -12,11 +12,11 @@ test('AsyncEffect.of(() -> a).inspect() outputs string AsyncEffect(a).', () => {
   expect(λ.AsyncEffect.of(() => '7turtle').inspect().includes('AsyncEffect(function')).toBe(true);
 });
 
-test('AsyncEffect.of(a -> b -> c -> d).trigger(e -> f)(g -> h)(i) for resolving curried function resolves.', done => {
+test('AsyncEffect.of(a -> b -> c).trigger(e -> f)(g -> h) for resolving curried function resolves.', done => {
   λ.AsyncEffect.of(resolving).trigger(error => error)(result => {
     expect(result).toBe('7urtle');
     done();
-  })('7urtle');
+  });
 });
 
 test('AsyncEffect.ofPromise(Promise) creates AsyncEffect from a promise.', done => {
@@ -24,7 +24,7 @@ test('AsyncEffect.ofPromise(Promise) creates AsyncEffect from a promise.', done 
   λ.AsyncEffect.ofPromise(successPromise).trigger(error => error)(result => {
     expect(result).toBe('7urtle');
     done();
-  })('7urtle');
+  });
 });
 
 test('AsyncEffect.ofPromise(Promise) rejects for a failed promise.', done => {
@@ -32,111 +32,111 @@ test('AsyncEffect.ofPromise(Promise) rejects for a failed promise.', done => {
   λ.AsyncEffect.ofPromise(failedPromise).trigger(error => {
     expect(error).toBe('error');
     done();
-  })(success => success)('7urtle');
+  })(success => success);
 });
 
-test('AsyncEffect.of((a, b, c) -> d).trigger(e -> f)(g -> h)(i) for resolving async ternary function resolves.', done => {
-  λ.AsyncEffect.of(resolvingTernary).trigger(error => error)(result => {
-    expect(result).toBe('7urtle');
-    done();
-  })('7urtle');
-});
-
-test('AsyncEffect.of((a -> b -> c -> d).trigger accepts ternary function.', done => {
-  λ.AsyncEffect.of(resolving).trigger(error => error, result => {
-    expect(result).toBe('7urtle');
-    done();
-  }, '7urtle');
-});
-
-test('AsyncEffect.of((a -> b -> c -> d).trigger accepts ternary function after map.', done => {
-  λ.AsyncEffect.of(resolving).map(a => a + 's').trigger(error => error, result => {
-    expect(result).toBe('7urtles');
-    done();
-  }, '7urtle');
-});
-
-test('AsyncEffect.of((a -> b -> c -> d).trigger accepts ternary function after flatMap.', done => {
-  λ.AsyncEffect.of(resolving).flatMap(a => λ.AsyncEffect.of(reject => resolve => value => resolve(a + 's'))).trigger(error => error, result => {
-    expect(result).toBe('7urtles');
-    done();
-  }, '7urtle');
-});
-
-test('AsyncEffect.of(a -> b -> c -> d).trigger(e -> f)(g -> h)(i) for rejecting curried function rejects.', done => {
-  λ.AsyncEffect.of(rejecting).trigger(error => {
-    expect(error).toBe('I am an error.');
-    done();
-  })(result => result)();
-});
-
-test('AsyncEffect.of((a, b, c) -> d).trigger(e -> f)(g -> h)(i) for rejecting async ternary function rejects.', done => {
-  λ.AsyncEffect.of(rejectingTernary).trigger(error => {
-    expect(error).toBe('I am an error.');
-    done();
-  })(result => result)();
-});
-
-test('AsyncEffect.of(a -> b -> c -> d).trigger(e -> f)(g -> h)(i) for synchronous exceptions rejects.', done => {
-  λ.AsyncEffect.of(throwing).trigger(error => {
-    expect(error).toBe('I am a thrown error.');
-    done();
-  })(result => result)();
-});
-
-test('AsyncEffect.of(a -> b -> c -> d).promise() outputs JavaScript promise.', done => {
-  λ.AsyncEffect.of(resolving).promise('7urtle').then(result => {
+test('AsyncEffect.of((a, b) -> c).trigger(d -> e)(f -> g) for resolving async binary function resolves.', done => {
+  λ.AsyncEffect.of(resolvingBinary).trigger(error => error)(result => {
     expect(result).toBe('7urtle');
     done();
   });
 });
 
-test('AsyncEffect.of(a -> b -> c -> d).map(b -> d) composes function over AsyncEffect input function.', done => {
+test('AsyncEffect.of((a -> b -> c).trigger accepts binary function.', done => {
+  λ.AsyncEffect.of(resolving).trigger(error => error, result => {
+    expect(result).toBe('7urtle');
+    done();
+  });
+});
+
+test('AsyncEffect.of((a -> b -> c).trigger accepts binary function after map.', done => {
+  λ.AsyncEffect.of(resolving).map(a => a + 's').trigger(error => error, result => {
+    expect(result).toBe('7urtles');
+    done();
+  });
+});
+
+test('AsyncEffect.of((a -> b -> c).trigger accepts binary function after flatMap.', done => {
+  λ.AsyncEffect.of(resolving).flatMap(a => λ.AsyncEffect.of(reject => resolve => resolve(a + 's'))).trigger(error => error, result => {
+    expect(result).toBe('7urtles');
+    done();
+  });
+});
+
+test('AsyncEffect.of(a -> b -> c).trigger(d -> e)(f -> g) for rejecting curried function rejects.', done => {
+  λ.AsyncEffect.of(rejecting).trigger(error => {
+    expect(error).toBe('I am an error.');
+    done();
+  })(result => result);
+});
+
+test('AsyncEffect.of((a, b) -> c).trigger(d -> e)(f -> g) for rejecting async binary function rejects.', done => {
+  λ.AsyncEffect.of(rejectingBinary).trigger(error => {
+    expect(error).toBe('I am an error.');
+    done();
+  })(result => result);
+});
+
+test('AsyncEffect.of(a -> b -> c).trigger(d -> e)(f -> g) for synchronous exceptions rejects.', done => {
+  λ.AsyncEffect.of(throwing).trigger(error => {
+    expect(error).toBe('I am a thrown error.');
+    done();
+  })(result => result);
+});
+
+test('AsyncEffect.of(a -> b -> c).promise() outputs JavaScript promise.', done => {
+  λ.AsyncEffect.of(resolving).promise().then(result => {
+    expect(result).toBe('7urtle');
+    done();
+  });
+});
+
+test('AsyncEffect.of(a -> b -> c).map(c -> d) composes function over AsyncEffect input function.', done => {
   λ.AsyncEffect.of(resolving).map(a => a + 's').trigger(error => error)(result => {
     expect(result).toBe('7urtles');
     done();
-  })('7urtle');
+  });
 });
 
-test('AsyncEffect.of(a -> b -> c -> d).flatMap(b -> AsyncEffect) outputs AsyncEffect.', done => {
-  λ.AsyncEffect.of(resolving).flatMap(a => λ.AsyncEffect.of(reject => resolve => value => resolve(a + 's'))).trigger(error => error)(result => {
+test('AsyncEffect.of(a -> b -> c).flatMap(c -> AsyncEffect) outputs AsyncEffect.', done => {
+  λ.AsyncEffect.of(resolving).flatMap(a => λ.AsyncEffect.of(reject => resolve => resolve(a + 's'))).trigger(error => error)(result => {
     expect(result).toBe('7urtles');
     done();
-  })('7urtle');
+  });
 });
 
-test('AsyncEffect.of(a -> b -> c -> d).flatMap(b -> AsyncEffect) handles reject states correctly passing along the initial error.', done => {
+test('AsyncEffect.of(a -> b -> c).flatMap(c -> AsyncEffect) handles reject states correctly passing along the initial error.', done => {
   let some = 1;
-  const resolving = reject => resolve => value => setTimeout(() => resolve(++some), 10);
-  const rejecting = reject => resolve => value => setTimeout(() => reject('I am an error.'), 10);
+  const resolving = reject => resolve => setTimeout(() => resolve(++some), 10);
+  const rejecting = reject => resolve => setTimeout(() => reject('I am an error.'), 10);
   λ.AsyncEffect.of(rejecting).flatMap(() => λ.AsyncEffect.of(resolving)).trigger(error => {
     expect(error).toBe('I am an error.');
     expect(some).toBe(1);
     done();
-  })(result => result)();
+  })(result => result);
 });
 
-test('AsyncEffect.of(a -> b -> c -> d).map(b -> d).ap(AsyncEffect) provides applicative ability to apply functors to each other.', done => {
+test('AsyncEffect.of(a -> b -> c).map(c -> d).ap(AsyncEffect) provides applicative ability to apply functors to each other.', done => {
   const add = a => b => a + b;
-  const resolving = reject => resolve => value => setTimeout(() => resolve(1), 10);
+  const resolving = reject => resolve => setTimeout(() => resolve(1), 10);
   λ.AsyncEffect.of(resolving).map(add).ap(λ.AsyncEffect.of(resolving)).trigger(error => error)(result => {
     expect(result).toBe(2);
     done();
-  })();
+  });
 });
 
 test('AsyncEffect.of(AsyncEffect -> AsyncEffect -> c).ap(AsyncEffect).ap(AsyncEffect) provides applicative interface for a functor of a function.', done => {
   const add = a => b => a + b;
-  const resolving = reject => resolve => value => setTimeout(() => resolve(1), 10);
-  λ.AsyncEffect.of(reject => resolve => value => resolve(add)).ap(λ.AsyncEffect.of(resolving)).ap(λ.AsyncEffect.of(resolving)).trigger(error => error)(result => {
+  const resolving = reject => resolve => setTimeout(() => resolve(1), 10);
+  λ.AsyncEffect.of(reject => resolve => resolve(add)).ap(λ.AsyncEffect.of(resolving)).ap(λ.AsyncEffect.of(resolving)).trigger(error => error)(result => {
     expect(result).toBe(2);
     done();
-  })();
+  });
 });
 
 test('No input function is executed until trigger is called.', done => {
   let some = 1;
-  const resolving = reject => resolve => value => setTimeout(() => resolve(++some), 10);
+  const resolving = reject => resolve => setTimeout(() => resolve(++some), 10);
   λ.AsyncEffect.of(resolving).flatMap(() => λ.AsyncEffect.of(resolving));
   setTimeout(() => {
     expect(some).toBe(3);
@@ -145,5 +145,5 @@ test('No input function is executed until trigger is called.', done => {
   λ.AsyncEffect.of(resolving).flatMap(() => λ.AsyncEffect.of(resolving)).trigger(error => error)(result => {
     expect(some).toBe(3);
     expect(result).toBe(3);
-  })();
+  });
 });
