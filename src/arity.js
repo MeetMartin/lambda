ed867fb0d10e1e4ccb66b46a32a7ae854692a16c
Martin Novak <ragnarecek@gmail.com>
2021-06-27 11:24:34 +1200

    new curry function

--- src/arity.js
+++ src/arity.js
@@ -1,5 +1,5 @@
 /**
- * nary takes input of a curried function and allows it to be called both as curried and n-ary.
+ * nary curried function and allows it to be called both as curried and n-ary.
  *
  * @HindleyMilner nary :: (a -> b) -> a -> b
  *
@@ -10,10 +10,30 @@
  * @example
  * import {nary} from '@7urtle/lambda';
  *
- * const fn1 = nary(a => b => a + b);
- * fn1('a')('b') === fn1('a', 'b'); // => true
+ * const fn = nary(a => b => a + b);
+ * fn('a')('b') === fn('a', 'b'); // => true
  */
 export const nary = fn =>
     (...args) => args.length === 0
         ? fn()
         : args.reduce((accumulator, current) => accumulator(current), fn);
+
+/**
+ * curry takes n-ary function and allows it to be called both as curried and n-ary.
+ *
+ * @HindleyMilner curry :: (a -> b) -> a -> b
+ *
+ * @pure
+ * @param {function} fn
+ * @return {function}
+ *
+ * @example
+ * import {curry} from '@7urtle/lambda';
+ *
+ * const fn = curry((a, b) => a + b);
+ * fn('a')('b') === fn('a', 'b'); // => true
+ */
+export const curry = fn =>
+    (...args) => args.length >= fn.length
+        ? fn.apply(null, args)
+        : (...args2) => curry(fn).apply(null, args.concat(args2));
--- tests/arity.test.js
+++ tests/arity.test.js
@@ -1,6 +1,13 @@
 import * as λ from '../src';
 
-test('nary takes input of a curried function and allows it to be called both as curried and n-ary.', () => {
+test('nary takes curried function and allows it to be called both as curried and n-ary.', () => {
     const fn1 = λ.nary(a => b => a + b);
     expect(fn1('a')('b')).toBe(fn1('a', 'b'));
+    expect(fn1('a')('b')).toBe('ab');
+});
+
+test('curry takes n-ary function and allows it to be called both as curried and n-ary.', () => {
+    const fn = λ.curry((a, b, c) => a + (b / c));
+    expect(fn(2, 4, 2)).toBe(fn(2)(4)(2));
+    expect(fn(2)(4)(2)).toBe(4);
 });
